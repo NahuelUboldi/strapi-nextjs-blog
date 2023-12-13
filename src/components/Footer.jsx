cd02e657304cd6a35102e6085ca668c21c4c6f09
Nahuel Uboldi <nahueluboldi@gmail.com>
2023-12-13 10:57:54 -0300

    footer finished

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer>
+      <div className='container'>
+        <hr />
+        <p>&copy; Headless Blog 2023</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
